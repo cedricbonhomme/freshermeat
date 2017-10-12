8bede5051410365b7708966252202b12396cfad9
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-10-12 23:25:25 +0200

    added a simple admin panel with Flask-Admin.

--- src/web/views/admin.py
+++ src/web/views/admin.py
@@ -1,14 +1,17 @@
 
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, current_app
+from flask_login import login_required, current_user
 from flask_paginate import Pagination, get_page_args
 from sqlalchemy import desc
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.menu import MenuLink
 
 from web.views.common import admin_permission
 from bootstrap import db
 from web import models
 
-admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
+admin_bp = Blueprint('admin_bp', __name__, url_prefix='/admin')
 
 
 @admin_bp.route('/dashboard', defaults={'per_page': '10'}, methods=['GET'])
@@ -43,3 +46,30 @@ def request(request_id=None):
 
 
     return render_template('admin/request.html', request=request)
+
+
+
+
+class UserView(ModelView):
+    column_exclude_list = ['pwdhash']
+    column_editable_list = ['email', 'firstname', 'lastname']
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_admin
+
+class ServiceView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_admin
+
+menu_link_back_dashboard = MenuLink(name='Back to dashboard',
+                             url='/admin/dashboard')
+
+admin_flask = Admin(current_app,
+                name='Management of data',
+                template_mode='bootstrap3',
+                index_view=AdminIndexView(
+                        name='Home',
+                        url='/admin/data_management'
+                    ))
+admin_flask.add_view(UserView(models.User, db.session))
+admin_flask.add_view(ServiceView(models.Service, db.session))
+admin_flask.add_link(menu_link_back_dashboard)
