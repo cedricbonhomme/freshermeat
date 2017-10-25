3e4a6bcbb293daf856f0bac6da025e7afabfd044
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-10-25 07:52:59 +0200

    fixes inactive transaction error

--- src/web/models/__init__.py
+++ src/web/models/__init__.py
@@ -22,34 +22,35 @@ def db_empty(db):
 
     # the transaction only applies if the DB supports
     # transactional DDL, i.e. Postgresql, MS SQL Server
-    trans = conn.begin()
+    with conn.begin() as trans:
 
-    inspector = reflection.Inspector.from_engine(db.engine)
+        inspector = reflection.Inspector.from_engine(db.engine)
 
-    # gather all data first before dropping anything.
-    # some DBs lock after things have been dropped in
-    # a transaction.
-    metadata = MetaData()
+        # gather all data first before dropping anything.
+        # some DBs lock after things have been dropped in
+        # a transaction.
+        metadata = MetaData()
 
-    tbs = []
-    all_fks = []
+        tbs = []
+        all_fks = []
 
-    for table_name in inspector.get_table_names():
-        fks = []
-        for fk in inspector.get_foreign_keys(table_name):
-            if not fk['name']:
-                continue
-            fks.append(ForeignKeyConstraint((), (), name=fk['name']))
-        t = Table(table_name, metadata, *fks)
-        tbs.append(t)
-        all_fks.extend(fks)
+        for table_name in inspector.get_table_names():
+            fks = []
+            for fk in inspector.get_foreign_keys(table_name):
+                if not fk['name']:
+                    continue
+                fks.append(ForeignKeyConstraint((), (), name=fk['name']))
+            t = Table(table_name, metadata, *fks)
+            tbs.append(t)
+            all_fks.extend(fks)
 
-    for fkc in all_fks:
-        conn.execute(DropConstraint(fkc))
+        for fkc in all_fks:
+            conn.execute(DropConstraint(fkc))
 
-    for table in tbs:
-        conn.execute(DropTable(table))
-    # trans.commit()
+        for table in tbs:
+            conn.execute(DropTable(table))
+
+        trans.commit()
 
 
 def db_init(db):
