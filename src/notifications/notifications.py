a8fea80255f1cfb0801eb472bb1b83409fe87ced
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-11-08 10:21:32 +0100

    Test the email templating.

--- src/notifications/notifications.py
+++ src/notifications/notifications.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, url_for
 from flask_mail import Message
 
-from bootstrap import mail
+from bootstrap import mail, application
 # from web.models import User
 
 
@@ -12,9 +12,13 @@ def new_request_notification(request):
     """New request notification.
     """
     subject = "[{service}] New request".format(service=request.service.name)
-    plaintext = render_template('emails/new_request.txt', request=request)
-
+    platform_url = application.config['SERVER_NAME'] + \
+                   url_for('admin_bp.view_request', request_id=request.id)
+    plaintext = render_template('emails/new_request.txt',
+                                request=request,
+                                platform_url=platform_url)
     msg = Message(subject,
                   recipients=[request.service.notification_email])
+
     msg.body = plaintext
     mail.send(msg)
