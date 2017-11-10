4ca4e9efcedeca5d508f2add444cfc3823471e73
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-11-10 21:43:41 +0100

    Bye shuffle. Organizations and services are now sorted alphabetically.

--- src/web/static/js/utils.js
+++ src/web/static/js/utils.js
@@ -12,10 +12,3 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
-
-function shuffle(a) {
-    for (let i = a.length; i; i--) {
-        let j = Math.floor(Math.random() * i);
-        [a[i - 1], a[j]] = [a[j], a[i - 1]];
-    }
-}
