149983f3236f55f5637298aafb4a0d4129f499b1
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-12-26 22:20:56 +0100

    Added page for the user profile.

--- src/web/views/user.py
+++ src/web/views/user.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
 
-from web.views.common import admin_permission
 from web.models import User
 
 
-user_bp = Blueprint('user_bp', __name__, url_prefix='/admin/user')
-users_bp = Blueprint('users_bp', __name__, url_prefix='/admin/users')
+user_bp = Blueprint('user_bp', __name__, url_prefix='/user')
+users_bp = Blueprint('users_bp', __name__, url_prefix='/users')
+
+
+@user_bp.route('/<string:nickname>', methods=['GET'])
+def get(nickname=None):
+    user = User.query.filter(User.nickname == nickname).first()
+    return render_template('user.html', user=user)
