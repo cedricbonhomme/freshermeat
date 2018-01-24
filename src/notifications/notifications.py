80d776333d53db57ad982d2188883563577c9c44
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-01-24 11:24:57 +0100

    Improved URL format.

--- src/notifications/notifications.py
+++ src/notifications/notifications.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import urllib.parse
 from flask import render_template, url_for
 from flask_mail import Message
 
@@ -10,10 +11,10 @@ from bootstrap import mail, application, instance_domain_name
 
 def new_request_notification(request):
     """New request notification.
-    """)
+    """
     subject = "[{service}] New request".format(service=request.project.name)
-    platform_url = instance_domain_name() + \
-                   url_for('admin_bp.view_request', request_id=request.id)
+    platform_url = urllib.parse.urljoin(instance_domain_name(),
+                   url_for('admin_bp.view_request', request_id=request.id))
     plaintext = render_template('emails/new_request.txt',
                                 request=request,
                                 platform_url=platform_url)
