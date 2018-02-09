32ee33ef95dbdfdf3ac55bc68d7d7675f6349a12
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-02-09 11:21:19 +0100

    Order result of api/v1/project by last_updated

--- src/web/views/api/v1/project.py
+++ src/web/views/api/v1/project.py
@@ -8,12 +8,20 @@ from web.views.api.v1 import processors
 from web.views.api.v1.common import url_prefix
 
 
+def pre_get_many(search_params=None, **kw):
+    order_by = [{"field":"last_updated","direction":"desc"}]
+    if 'order_by' not in search_params:
+        search_params['order_by'] = []
+    search_params['order_by'].extend(order_by)
+
+
 blueprint_project = manager.create_api_blueprint(
     models.Project,
     url_prefix=url_prefix,
     methods=['GET', 'POST', 'PUT', 'DELETE'],
     exclude_columns=['requests', 'notification_email'],
     preprocessors=dict(
+        GET_MANY=[pre_get_many],
         POST=[processors.auth_func],
         PUT=[processors.auth_func],
         DELETE=[processors.auth_func]))
