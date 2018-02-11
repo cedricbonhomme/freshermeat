30f643a1143c7ca54ecb5ba8f74ed378beb09081
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-02-11 21:51:15 +0100

    Added a view which provides JSON data for the statistics page. The statistics pages actually uses Frappe Charts.

--- src/web/views/__init__.py
+++ src/web/views/__init__.py
@@ -5,9 +5,10 @@ from web.views.user import user_bp, users_bp
 from web.views.project import project_bp, projects_bp
 from web.views.organization import organization_bp, organizations_bp
 from web.views.service import service_bp
+from web.views.stats import stats_bp
 
 __all__ = ['v1', 'views', 'session_mgmt', 'admin_bp',
            'user_bp', 'users_bp',
            'project_bp', 'projects_bp',
            'organization_bp', 'organizations_bp',
-           'service_bp']
+           'service_bp', 'stats_bp']
---
+++ src/web/views/stats.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, jsonify
+
+from sqlalchemy import func
+from bootstrap import db
+from web.models import Project, License
+
+stats_bp = Blueprint('stats_bp', __name__,
+                       url_prefix='/stats')
+
+
+@stats_bp.route('/', methods=['GET'])
+def stats():
+    return render_template('stats.html')
+
+
+@stats_bp.route('/licenses.json', methods=['GET'])
+def licenses(service_name=None):
+    result = db.session.query(License.name, func.count(License.id)). \
+                              join(License.projects).group_by(License.id).all()
+    return jsonify(dict(result))
