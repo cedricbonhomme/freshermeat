cbefe416850b3835ae31bccf08d3adf07f140aff
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-02-15 21:21:38 +0100

    Generates releases feed per orgaizations.

--- src/web/views/organization.py
+++ src/web/views/organization.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, request, abort
+from werkzeug.contrib.atom import AtomFeed
 
+from bootstrap import db
 from web.models import Organization
 
 organization_bp = Blueprint('organization_bp', __name__,
@@ -19,3 +21,21 @@ def get(organization_name=None):
     if organization is None:
         abort(404)
     return render_template('organization.html', organization=organization)
+
+
+@organization_bp.route('/<string:organization_name>/releases.atom', methods=['GET'])
+def recent_releases(organization_name=None):
+    """Generates a feed for the releases."""
+    organization = Organization.query. \
+                        filter(Organization.name==organization_name).first()
+    if organization is None:
+        abort(404)
+    feed = AtomFeed('Recent releases for {}'.format(organization.name),
+                     feed_url=request.url, url=request.url_root)
+    for project in organization.projects:
+        for release in project.releases:
+            feed.add(release.version, release.changes,
+                     id=release.id,
+                     url=release.release_url,
+                     updated=release.published_at)
+    return feed.get_response()
