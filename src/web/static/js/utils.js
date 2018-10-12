858f2a2eb6bb8709f6ec680e3e71c9d04aca1ebd
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-10-12 23:41:34 +0200

    Factorized code for the generation of pastel colors.

--- src/web/static/js/utils.js
+++ src/web/static/js/utils.js
@@ -23,3 +23,18 @@ function updateQueryStringParameter(uri, key, value) {
         return uri + separator + key + "=" + value;
     }
 }
+
+// random pastel colors
+function pastelColorMaker() {
+    var hue = Math.floor(Math.random() * 360);
+    return 'hsl(' + hue + ', 100%, 87.5%)';
+}
+// some static pastel colors
+var colors = ['rgba(230, 25, 75, 0.2)', 'rgba(60, 180, 75, 0.2)',
+    'rgba(255, 225, 25, 0.2)', 'rgba(0, 130, 200, 0.2)', 'rgba(245, 130, 48, 0.2)',
+    'rgba(145, 30, 180, 0.2)', 'rgba(70, 240, 240, 0.2)', 'rgba(240, 50, 230, 0.2)',
+    'rgba(210, 245, 60, 0.2)', 'rgba(250, 190, 190, 0.2)', 'rgba(0, 128, 128, 0.2)',
+    'rgba(230, 190, 255, 0.2)', 'rgba(170, 110, 40, 0.2)', 'rgba(255, 250, 200, 0.2)',
+    'rgba(128, 0, 0, 0.2)', 'rgba(170, 255, 195, 0.2)', 'rgba(128, 128, 0, 0.2)',
+    'rgba(255, 215, 180, 0.2)', 'rgba(0, 0, 128, 0.2)', 'rgba(128, 128, 128, 0.2)',
+    'rgba(255, 255, 255, 0.2)','rgba(0, 0, 0, 0.2)'];
