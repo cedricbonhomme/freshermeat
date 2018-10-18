80356dd2443611f7d76aa1d12d506f7656c9aa46
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-10-18 22:27:39 +0200

    Various improvements to the sorting of the projects and organizations.

--- src/web/views/api/v1/organization.py
+++ src/web/views/api/v1/organization.py
@@ -26,11 +26,19 @@ from web.views.api.v1 import processors
 from web.views.api.v1.common import url_prefix
 
 
+def pre_get_many(search_params=None, **kw):
+    order_by = [{"field":"last_updated", "direction":"desc"}]
+    if 'order_by' not in search_params:
+        search_params['order_by'] = []
+    search_params['order_by'].extend(order_by)
+
+
 blueprint_organization = manager.create_api_blueprint(
     models.Organization,
     url_prefix=url_prefix,
     methods=['GET', 'POST', 'PUT', 'DELETE'],
     preprocessors=dict(
+        GET_MANY=[pre_get_many],
         POST=[processors.auth_func],
         PUT=[processors.auth_func],
         DELETE=[processors.auth_func],
