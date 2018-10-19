e954bad3f95aaa3b8d72803ab4849e81200fb887
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-10-19 07:42:35 +0200

    Fixed a bug where the published_at attribute of a CVE wasn't updated on creation

--- src/web/models/cve.py
+++ src/web/models/cve.py
@@ -9,7 +9,7 @@ class CVE(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cve_id = db.Column(db.String(), nullable=False)
     summary = db.Column(db.String(), default='')
-    published_at = db.Column(db.DateTime(), default=datetime.utcnow())
+    published_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
     # foreign keys
     project_id = db.Column(db.Integer(), db.ForeignKey('project.id'),
