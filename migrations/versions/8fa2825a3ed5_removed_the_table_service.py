5d5a3aff96765235c69ccb49937caa36f619847c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-08-25 15:11:18 +0200

    new: [database] added migration script to remove services and requests objects from the database

---
+++ migrations/versions/8fa2825a3ed5_removed_the_table_service.py
@@ -0,0 +1,21 @@
+"""Removed the table service
+
+Revision ID: 8fa2825a3ed5
+Revises: ca38dde2c84e
+Create Date: 2019-08-25 14:42:15.555968
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8fa2825a3ed5'
+down_revision = 'ca38dde2c84e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_table('service')
+    op.drop_table('request')
