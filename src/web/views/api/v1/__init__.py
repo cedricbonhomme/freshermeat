c86de70a452c64e517dbe0102f0be26158bbd719
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-08-25 14:38:07 +0200

    chg: [api] Removed endpoint related to the services

--- src/web/views/api/v1/__init__.py
+++ src/web/views/api/v1/__init__.py
@@ -5,7 +5,6 @@ from web.views.api.v1.code import blueprint_code
 from web.views.api.v1.tag import blueprint_tag
 from web.views.api.v1.release import blueprint_release
 from web.views.api.v1.cve import blueprint_cve
-from web.views.api.v1.service import blueprint_service
 from web.views.api.v1.request import blueprint_request
 from web.views.api.v1.license import blueprint_license
 from web.views.api.v1.language import blueprint_language
@@ -14,5 +13,5 @@ from web.views.api.v1.feed import blueprint_feed
 
 __all__ = [blueprint_organization, blueprint_project, blueprint_cve,
            blueprint_request, blueprint_release, blueprint_user,
-           blueprint_code, blueprint_service, blueprint_license,
+           blueprint_code, blueprint_license,
            blueprint_language, blueprint_news, blueprint_feed]
